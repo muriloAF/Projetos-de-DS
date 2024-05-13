20219d369dc981a542705420c0f73dab39800fdc
muriloAF <163204469+muriloAF@users.noreply.github.com>
2024-05-12 21:17:12 -0300

    ProjAlunoOO
    O objetivo é armazenar as informações do aluno em uma classe e mostra-las nos campos.

---
+++ ProjAlunoOO/src/Aluno.java
@@ -0,0 +1,7 @@
+
+public class Aluno {
+    public String nome;
+    public int rm;
+    public double valormens;
+    public int idade;
+}
---
+++ ProjAlunoOO/src/JoptionPane.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 23298
+ */
+class JoptionPane {
+
+    static void showMessagDialog(Object object, String danos_Cadastrados_) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void showMessageDialog(Object object, String danos_Cadastrados_) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
